eb3550a2e99dc3ba009ecc5ceed865d7ee40e108
amosoisin <szbh.itoshiki.izaya@gmail.com>
2020-01-06 19:21:42 +0900

    adjust time in 30s

--- suggest_parts/search_max_score.py
+++ suggest_parts/search_max_score.py
@@ -16,7 +16,7 @@ class SearchMaxScore:
         self.init_dataset()
         self.init_model()
         self.budget = budget
-        self.GENE_NUM = 1000
+        self.GENE_NUM = 500
         self.family = []
         self.max_score_list = []
 
